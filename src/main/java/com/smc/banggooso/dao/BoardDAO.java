df4a85e9cb24a344ea990d2af679a10142b201c8
mina-jo <59588352+mina-jo@users.noreply.github.com>
2021-04-28 16:04:08 +0900

    Revert "commit"
    This reverts commit da7dfff4aac5ceb6ea77546d1630090f06251409.

--- src/main/java/com/smc/banggooso/dao/BoardDAO.java
+++
@@ -1,6 +0,0 @@
-package com.smc.banggooso.dao;
-
-import com.smc.banggooso.vo.Board;
-
-public interface BoardDAO  {
-}
--- src/main/java/com/smc/banggooso/service/board/BoardService.java
+++
@@ -1,6 +0,0 @@
-package com.smc.banggooso.service.board;
-
-public interface BoardService {
-
-
-}
